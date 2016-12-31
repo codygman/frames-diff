436a6fcabe7cbee60696bfbdbafb6fbc0381717f
Cody Goodman <codygman.consulting@gmail.com>
2016-12-31 16:13:12 -0600

    add test todo

---
+++ test/Test.hs
@@ -0,0 +1,13 @@
+-- TODO
+-- tableTypes' rowGen { rowTypeName = "A"} "data/a.txt"
+-- tableTypes' rowGen { rowTypeName = "B"} "data/b.txt"
+
+-- as :: Producer A IO ()
+-- as = readTable "data/a.txt"
+
+-- bs :: Producer B IO ()
+-- bs = readTable "data/b.txt"
+
+-- singA =  head . F.toList <$> inCoreAoS as
+
+-- singB = head . F.toList <$> inCoreAoS bs
