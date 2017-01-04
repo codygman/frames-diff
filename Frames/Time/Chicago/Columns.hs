94b1e17adfa01bb60ab51e81d56ae6712c81cd01
Cody Goodman <codygman.consulting@gmail.com>
2017-01-04 01:08:34 -0600

    automatically derive Ord/Eq instances for Chicago/chain

--- Frames/Time/Chicago/Columns.hs
+++ Frames/Time/Chicago/Columns.hs
@@ -45,6 +45,12 @@ newtype Chicago = Chicago (TimeIn "America/Chicago") deriving (Show)
 instance Parseable Chicago where
   parse = fmap (fmap Chicago) . parse
 
+-- TODO add some tests to make sure these act sanely
+deriving instance Eq (TimeIn "America/Chicago")
+deriving instance Ord ZonedTime
+deriving instance Ord (TimeIn "America/Chicago")
+deriving instance Ord Chicago
+
 instance Eq Chicago where
    (Chicago (TimeIn zndTm)) == (Chicago (TimeIn zndTm')) = zndTm == zndTm'
 
