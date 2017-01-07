7393d598e6cc0668b6da3011dd228574c33a7778
Cody Goodman <codygman.consulting@gmail.com>
2017-01-07 02:25:42 -0600

    update bench

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,10 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
 import Criterion.Main
 import Frames.ColumnTypeable 
 import Frames.Time.LocalTime
 import qualified Data.Text as T
 import Data.Thyme
 import Control.Monad
+import Control.DeepSeq
+import GHC.Generics
+
+
+-- deriving instance Generic LocalTime
+-- deriving instance NFData LocalTime
 
 -- The function we're benchmarking.
 fib m | m < 0     = error "negative!"
@@ -15,14 +24,15 @@ fib m | m < 0     = error "negative!"
     go n = go (n-1) + go (n-2)
 
 -- testParse1e6 :: MonadPlus m => [m (Parsed LocalTime)]
-testParse1e6 :: Int -> [Maybe (Parsed LocalTime)]
-testParse1e6 n = map parse (Prelude.replicate n ("2016-05-03 00:00:00 +0006 CST" :: T.Text))
+testParse1e6 :: Int -> [Maybe LocalTime]
+testParse1e6 n = map fuzzyParseLocalTime (Prelude.replicate n ("2016-05-03 00:00:00 +0006 CST" :: T.Text))
 
 -- Our benchmark harness.
 main = defaultMain [
-  bgroup "date parsing" [ bench "1000"  $ whnf testParse1e6 1000
-                        , bench "10000"  $ whnf testParse1e6 10000
-                        , bench "100000"  $ whnf testParse1e6 10000
-                        , bench "1000000"  $ whnf testParse1e6 100000
+  bgroup "date parsing" [ bench "1000 nf"  $ nf testParse1e6 1000
+                          bench "1000 whnf"  $ whnf testParse1e6 1000
+                        -- , bench "10000"  $ whnf testParse1e6 10000
+                        -- , bench "100000"  $ whnf testParse1e6 10000
+                        -- , bench "1000000"  $ whnf testParse1e6 100000
                         ]
   ]
