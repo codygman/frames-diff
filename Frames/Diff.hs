8efc8a90d65cc7d409f1eeecb5b12cb9a397cd23
Cody Goodman <codygman.consulting@gmail.com>
2017-01-12 23:23:27 -0600

    add non-working innerJoin update

--- Frames/Diff.hs
+++ Frames/Diff.hs
@@ -16,7 +16,8 @@ module Frames.Diff ( defaultingProducer
                    , Default(..)
                    , dt
                    , distinctOn
-                   , innerJoin
+                   -- , innerJoin
+                   -- , innerJoin'
                    , takeFrame
                    ) where
 
@@ -38,6 +39,7 @@ import qualified Data.Map.Strict as M
 import qualified Data.Text.Format as T
 import Data.Int (Int8)
 import Data.Time
+import Data.Proxy
 import qualified Data.HashSet as HS
 import Data.Foldable as F
 import Control.Monad.Primitive (PrimMonad)
@@ -108,26 +110,122 @@ distinctOn lens1 rowProducer = do
 -- Recommend keeping columns in producers disjoint because accessing
 -- anything but the leftmost duplicate column could prove difficult.
 -- see: https://github.com/VinylRecords/Vinyl/issues/55#issuecomment-269891633
-innerJoin
-  :: (RecVec (as ++ bs), PrimMonad m, Ord k) =>
-     Producer (Rec Identity as) m ()
-     -> Getting k (Rec Identity as) k
-     -> Producer (Rec Identity bs) m ()
-     -> Getting k (Rec Identity bs) k
-     -> m (FrameRec (as ++ bs))
+innerJoin :: (MonadIO m, Ord k) =>
+             Producer (Rec f leftRows) IO ()  -- leftProducer
+          -> Getting k (Rec f leftRows) k     -- leftProducer lens
+          -> Producer (Rec f rightRows) IO () -- rightProducer
+          -> Getting k (Rec f rightRows) k    -- rightProducer lens
+          -> m (P.Proxy P.X () () (Rec f (leftRows ++ rightRows)) IO ())
 innerJoin leftProducer leftLens rightProducer rightLens = do
-  -- build a Map with values from the left producer
-  leftKeyMap <- P.fold (\m r -> M.insert (view leftLens r) r m) M.empty id leftProducer
-  inCoreAoS (rightProducer
-             >-> P.filter (\r -> M.member (view rightLens r) leftKeyMap)
-             >-> P.map (\row -> do
-                          case M.lookup (view rightLens row) leftKeyMap of
-                            Just leftRow -> do
-                              rappend leftRow row
-                            Nothing -> error "this shouldn't happen"
-                           )
-            )
+  leftProducerLen <- P.liftIO $ P.length leftProducer
+  rightProducerLen <- P.liftIO $ P.length rightProducer
 
+  let curProducer = case rightProducerLen < leftProducerLen of
+                      True -> rightProducer
+                      -- False -> leftProducer
+
+  let curKeymapProducer = case rightProducerLen < leftProducerLen of
+                            True -> leftProducer
+                            -- False -> rightProducer
+
+  let curLensLookup = case rightProducerLen < leftProducerLen of
+                  True -> rightLens
+                  -- False -> leftLens
+
+  let curLensInsert = case rightProducerLen < leftProducerLen of
+                  True -> leftLens
+                  -- False -> rightLens
+
+
+  let appender km row = case rightProducerLen < leftProducerLen of
+                          True -> case M.lookup (view curLensLookup row) km of
+                                     Just otherRow -> pure $ rappend otherRow row
+                                     Nothing -> P.mzero
+                          -- False -> case M.lookup (view curLensLookup row) km of
+                          --            Just otherRow -> pure $ rappend row otherRow
+                          --            Nothing -> P.mzero
+
+  keyMap <- P.liftIO $ P.fold (\m r -> M.insert (view curLensInsert r) r m) M.empty id curKeymapProducer
+
+  pure $ curProducer >-> P.mapM (\r -> appender keyMap r)
+
+-- error if I uncomment my false cases (specifically the False case  for curProducer)
+-- [5 of 5] Compiling Frames.Diff      ( Frames/Diff.hs, interpreted )
+
+-- Frames/Diff.hs:125:32: error:
+--     • Couldn't match type ‘leftRows’ with ‘rightRows’
+--       ‘leftRows’ is a rigid type variable bound by
+--         the type signature for:
+--           innerJoin :: forall (m :: * -> *) k (f :: *
+--                                                     -> *) (leftRows :: [*]) (rightRows :: [*]).
+--                        (MonadIO m, Ord k) =>
+--                        Producer (Rec f leftRows) IO ()
+--                        -> Getting k (Rec f leftRows) k
+--                        -> Producer (Rec f rightRows) IO ()
+--                        -> Getting k (Rec f rightRows) k
+--                        -> m (P.Proxy P.X () () (Rec f (leftRows ++ rightRows)) IO ())
+--         at Frames/Diff.hs:113:14
+--       ‘rightRows’ is a rigid type variable bound by
+--         the type signature for:
+--           innerJoin :: forall (m :: * -> *) k (f :: *
+--                                                     -> *) (leftRows :: [*]) (rightRows :: [*]).
+--                        (MonadIO m, Ord k) =>
+--                        Producer (Rec f leftRows) IO ()
+--                        -> Getting k (Rec f leftRows) k
+--                        -> Producer (Rec f rightRows) IO ()
+--                        -> Getting k (Rec f rightRows) k
+--                        -> m (P.Proxy P.X () () (Rec f (leftRows ++ rightRows)) IO ())
+--         at Frames/Diff.hs:113:14
+--       Expected type: Producer (Rec f rightRows) IO ()
+--         Actual type: Producer (Rec f leftRows) IO ()
+--     • In the expression: leftProducer
+--       In a case alternative: False -> leftProducer
+--       In the expression:
+--         case rightProducerLen < leftProducerLen of {
+--           True -> rightProducer
+--           False -> leftProducer }
+--     • Relevant bindings include
+--         curProducer :: Producer (Rec f rightRows) IO ()
+--           (bound at Frames/Diff.hs:123:7)
+--         rightLens :: Getting k (Rec f rightRows) k
+--           (bound at Frames/Diff.hs:119:47)
+--         rightProducer :: Producer (Rec f rightRows) IO ()
+--           (bound at Frames/Diff.hs:119:33)
+--         leftLens :: Getting k (Rec f leftRows) k
+--           (bound at Frames/Diff.hs:119:24)
+--         leftProducer :: Producer (Rec f leftRows) IO ()
+--           (bound at Frames/Diff.hs:119:11)
+--         innerJoin :: Producer (Rec f leftRows) IO ()
+--                      -> Getting k (Rec f leftRows) k
+--                      -> Producer (Rec f rightRows) IO ()
+--                      -> Getting k (Rec f rightRows) k
+--                      -> m (P.Proxy P.X () () (Rec f (leftRows ++ rightRows)) IO ())
+--           (bound at Frames/Diff.hs:119:1)
+
+
+
+type UserId = "userId" :-> Int
+type Simple1 = Record '["auto" :-> Int, UserId]
+type Simple2 = Record '[UserId, "uslessCol" :-> Text]
+userId ::
+  forall f rs. (Functor f, UserId ∈ rs) =>
+  (Int -> f Int) -> Record rs -> f (Record rs)
+userId = rlens (Proxy :: Proxy UserId)
+
+simple1 :: [Simple1]
+simple1 = [ 0 &: 100 &: Nil
+          , 1 &: 100 &: Nil
+          , 2 &: 100 &: Nil
+          , 3 &: 100 &: Nil
+          ]
+
+simple2 :: [Simple2]
+simple2 = [ 100 &: "usless" &: Nil]
+
+
+-- expect to get 4 rows regardless of whether simple1 or simple2 is on left side of join
+
+-- runEffect $ innerJoin' (P.each simple1) userId (P.each simple2) userId  >-> P.print
 
 -- | turns a Frame into a list and takes n elements from it
 -- TODO is there a more efficient way?? Subset slicing works in this case.
